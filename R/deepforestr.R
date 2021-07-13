1e4183a9b0d2fefc27a15e3e9fe305a55a2bcb40
henrykironde <henrykironde@gmail.com>
2021-07-13 08:07:31 -0400

    Add get_data

--- R/deepforestr.R
+++ R/deepforestr.R
@@ -1,3 +1,17 @@
+#' Check for updates
+#'
+#' @param repo path to the repository
+#'
+#' @examples
+#' \donttest{
+#' deepforest::get_data()
+#' }
+#' @importFrom reticulate import r_to_py
+#' @export
+get_data <- function(repo = "") {
+  writeLines(strwrap("Please wait while the retriever updates its scripts, ..."))
+  deepforest$get_data(repo)
+}
 
 # global reference to python modules (will be initialized in .onLoad)
 deepforest <- NULL
