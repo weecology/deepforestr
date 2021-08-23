0a3e2275186d7d24cfbc8ba809d6198399668f93
Ethan White <ethan@weecology.org>
2021-08-23 15:05:12 -0400

    Add basic integration tests

---
+++ tests/testthat/integration-tests.R
@@ -0,0 +1,41 @@
+library(testthat)
+
+context("integration tests for DeepForest in R")
+
+test_that("deepforest model exists when loaded", {
+
+  # download is held back on cran
+
+    skip_on_cran() 
+  
+  model = df_model()
+  expect_type(model, "closure")
+
+})
+
+test_that("use_release model exists when loaded", {
+
+  # download is held back on cran
+
+    skip_on_cran() 
+  
+  model = df_model()
+  model$use_release()
+  expect_type(model, "closure")
+
+})
+
+test_that("image prediction works", {
+
+  # download is held back on cran
+
+    skip_on_cran() 
+  
+  expect
+
+  model = df_model()
+  model$use_release()
+  image_path = get_data("OSBS_029.png")
+  bounding_boxes = model$predict_image(path=image_path, return_plot=FALSE)
+  expect_identical(nrow(bounding_boxes), 56)
+})
