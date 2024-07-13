7c1d22954c33bfe944322bbc0dc0da762c746267
Ethan White <ethan@weecology.org>
2024-07-13 10:58:59 -0400

    Skip tests if deepforest not present instead of skipping on CRAN
    This lets tests run on CI and follows recommend practice:
    https://rstudio.github.io/reticulate/articles/package.html#checking-and-testing-on-cran

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -2,47 +2,33 @@ library(testthat)
 
 context("integration tests for DeepForest in R")
 
+skip_if_no_deepforest <- function() {
+  deepforest_avail <- reticulate::py_module_available("deepforest")
+  if (!deepforest_avail)
+    skip("deepforest not available for testing")
+}
+
 test_that("deepforest model is installed", {
-    
-    # Python package not installed on CRAN
-
-    skip_on_cran() 
-
+  skip_if_no_deepforest()
   deepforest_available = reticulate::py_module_available("deepforest")
   expect_identical(deepforest_available, TRUE)
 })
 
 test_that("deepforest model exists when loaded", {
-
-  # Python package not installed on CRAN
-
-    skip_on_cran() 
-  
+  skip_if_no_deepforest()
   model = df_model()
   expect_type(model, "closure")
-
 })
 
 test_that("use_release model exists when loaded", {
-
-  # Python package not installed on CRAN
-
-    skip_on_cran() 
-  
+  skip_if_no_deepforest()
   model = df_model()
   model$use_release()
   expect_type(model, "closure")
-
 })
 
 test_that("image prediction works", {
-
-  # Python package not installed on CRAN
-
-    skip_on_cran() 
-  
-  expect
-
+  skip_if_no_deepforest()
   model = df_model()
   model$use_release()
   image_path = get_data("OSBS_029.png")
@@ -51,9 +37,7 @@ test_that("image prediction works", {
 })
 
 test_that("training works", {
-
-  skip_on_cran()
-
+  skip_if_no_deepforest()
   model = df_model()
   model$use_release()
   annotations_file = get_data("testfile_deepforest.csv")
